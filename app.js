83e35f8e6729701ec59bdea85c204154bf2cc7d0
Prakash Naikwadi <naikwadiprakash1@gmail.com>
2022-12-21 23:30:38 +0530

    GET and POST request routes added

---
+++ app.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+
+// middleware (between req and res)
+app.use(express.json()); // this middleware adds req data to req.body
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}...`);
+});
