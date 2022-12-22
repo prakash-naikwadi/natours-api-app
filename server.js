67d1ee091e0866d3e89ce346405e65b22dc41852
Prakash Naikwadi <naikwadiprakash1@gmail.com>
2022-12-23 00:24:22 +0530

    mongoose package install, mongodb database connection setup

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-console.log(app.get('env'));
-
 const port = process.env.PORT || 3000;
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('db connection successfully');
+  });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}...`);
