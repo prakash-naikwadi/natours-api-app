6cfbd444421109ddc954bf8fc4084e974dd5457b
Prakash Naikwadi <naikwadiprakash1@gmail.com>
2022-12-26 21:55:52 +0530

    tour model new fields added

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,15 +5,51 @@ const tourSchema = mongoose.Schema({
     type: String,
     required: [true, 'A tour must have Name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    require: [true, 'A tour must have Durations.'],
+  },
+  maxGroupSize: {
+    type: Number,
+    require: [true, 'A tour must have max group size'],
+  },
+  difficulty: {
+    type: String,
+    require: [true, 'A tour must have difficulty level'],
+  },
+  ratingAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+  },
+  description: {
+    type: String,
+    require: [true, 'A tour must have description'],
+  },
+  imageCover: {
+    type: String,
+    require: [true, 'A tour must have cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
