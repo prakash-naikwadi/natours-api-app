4af6a8d383d7f942a20a782f85b5b8e4bc7c27ac
Prakash Naikwadi <naikwadiprakash1@gmail.com>
2022-12-27 17:55:16 +0530

    Filtering , advanced filtering, sorting, limiting fields added

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -1,9 +1,44 @@
+const { query } = require('express');
 const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    // 1A) Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
 
+    // 1B) Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+
+    queryStr = queryStr.replace(/\b(lt|lte|gt|gte)\b/g, (match) => `$${match}`);
+
+    const query = Tour.find(JSON.parse(queryStr)); // this returns query and we then can chain other methods to it
+
+    // 2) Sorting
+    if (req.query.sort) {
+      let sortStr = req.query.sort;
+      sortStr = sortStr.split(',').join(' ');
+      console.log(sortStr);
+      query.sort(sortStr);
+    } else {
+      query.sort('-createdAt');
+    }
+
+    // 3) Limiting fields
+    if (req.query.fields) {
+      console.log(req.query.fields);
+      const fields = req.query.fields.split(',').join(' ');
+      query.select(fields);
+    } else {
+      query.select('-__v');
+    }
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       data: {
